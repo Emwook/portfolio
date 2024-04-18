3f6f9b1560fc55e4a38944bafd0810b1ccc764ab
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-04-18 11:54:46 +0200

    project setup with TS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Container className='mt-5 text-secondary'>
+        <h1>
+          Portfolio site
+        </h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
